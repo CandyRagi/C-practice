9f71edba341e96203cf19690435dffa30456972c
CandyRagi <tiwariansh935@gmail.com>
2023-09-06 16:50:39 -0700

    second time on first day

--- class work/1.cpp
+++ class work/1.cpp
@@ -4,6 +4,20 @@ using namespace std;
 
 int main(){
 
+    int age;
+    float price;
+    double volume;
+
+    age=25;
+    price=1.15;
+    volume=3.75;
+
+    cout<<"I am "<<age<<"years old. "<<endl;
+    cout<<"The price of oranges is $"<<price<<" per kilogram."<<endl;
+    cout<<"The volume of milk is "<<volume<<endl;
+
+
+
     return 0;
 
 }
